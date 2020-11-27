4385de75eceb8c07b79c323a49fbcd5c014e149d
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2020-11-27 19:46:39 -0300

    Update netwoklayer.dart

--- lib/netwoklayer.dart
+++ lib/netwoklayer.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 Future<String> fetchCountry(http.Client client) async {
-  final response = await client.get('https://youhost.com.br/json4.php');
+  final response = await client.get('https://seu_host.com.br/api');
   // Use the compute function to run parsePhotos in a separate isolate
   return response.body;
 }
